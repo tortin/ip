c19e46d01f8d21c444cf4997eda05454be0d9c12
Wei Jian <lowweijian12@gmail.com>
2026-01-22 19:09:51 +0800

    Add, List

--- src/main/java/Pablo.java
+++ src/main/java/Pablo.java
@@ -1,24 +1,40 @@
 import java.util.Scanner;
+import java.lang.StringBuilder;
 
 public class Pablo {
+    public static String list_tasks(String[] tasks, int len) {
+        StringBuilder output = new StringBuilder();
+        for (int i = 1; i <= len; i++) {
+            output.append(String.format("%d: %s\n", i, tasks[i - 1]));
+        }
+        return output.toString();
+    }
+
+    public static void print_resp(String msg) {
+        System.out.println("____________________________________________________________");
+        System.out.println(msg);
+        System.out.println("____________________________________________________________");
+    }
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Pablo!");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        String[] tasks = new String[100];
+        int task_idx = 0;
+
+        print_resp("Hello! I'm Pablo!\nWhat can I do for you?");
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________");
+            if (!input.equals("list")) {
+                tasks[task_idx] = input;
+                task_idx++;
+                print_resp("added: " + input);
+            } else {
+                print_resp(list_tasks(tasks, task_idx));
+            }
             input = scanner.nextLine();
         }
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        print_resp("Bye. Hope to see you again soon!");
     }
 }

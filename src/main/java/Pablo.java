c5efaba6ba5b9ef526856f04709bbf10229d7484
Wei Jian <lowweijian12@gmail.com>
2026-01-22 18:36:05 +0800

    Echo

--- src/main/java/Pablo.java
+++ src/main/java/Pablo.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Pablo {
     public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Pablo!");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+            input = scanner.nextLine();
+        }
+
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }

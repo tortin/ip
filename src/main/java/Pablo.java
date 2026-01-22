66e25c8c66a44dc4b6471597ccbd56d3db908762
Wei Jian <lowweijian12@gmail.com>
2026-01-22 18:20:30 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Pablo.java
@@ -0,0 +1,7 @@
+public class Pablo {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Pablo!");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}

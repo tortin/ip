c8e1b24964b6f9db7230ac299dae74b145d6bf49
Wei Jian <lowweijian12@gmail.com>
2026-02-10 01:12:05 +0800

    Fix tests

--- src/test/java/pablo/parser/ParserTest.java
+++ src/test/java/pablo/parser/ParserTest.java
@@ -1,15 +1,14 @@
 package pablo.parser;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.time.LocalDateTime;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pablo.command.AddCommand;
 import pablo.command.Command;
+import pablo.command.CommandResult;
 import pablo.command.NullCommand;
+import pablo.fileio.DataLoader;
 import pablo.task.Deadline;
+import pablo.task.TaskList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
@@ -17,40 +16,32 @@ import static pablo.parser.Parser.parseDeadline;
 
 public class ParserTest {
 
-    private final PrintStream originalOut = System.out;
-    private ByteArrayOutputStream outContent;
-
-    @BeforeEach
-    void setUp() {
-        outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-    }
-
-    @AfterEach
-    void tearDown() {
-        System.setOut(originalOut);
-    }
+    private TaskList dummyTaskList = new TaskList();
+    private DataLoader dummyStorage = new DataLoader("./data/dummy_data.txt");
 
     @Test
     public void deadlineParser_success(){
         Command c = parseDeadline("deadline return book /by 06-06-2026_12:00");
         AddCommand add = (AddCommand) c;
         Deadline d = (Deadline) add.getTask();
-        assertEquals("return book", d.getName());
-        assertEquals(LocalDateTime.of(2026, 6, 6, 12, 0), d.getBy());
+        assertEquals(new Deadline("return book", false, LocalDateTime.of(2026, 6, 6, 12, 0)), d);
     }
 
     @Test
     public void deadlineParser_dateFormatError_caught() {
         Command c = Parser.parse("deadline submit report /by nonsense");
         assertInstanceOf(NullCommand.class, c);
-        assertEquals("Date and time information must be specified using dd-mm-yyyy_hh:mm\r\n", outContent.toString());
+        CommandResult cr = c.execute(dummyTaskList, dummyStorage);
+        assertEquals("Date and time information must be specified using dd-mm-yyyy_hh:mm",
+                cr.getResponse());
     }
 
     @Test
     public void deadlineParser_indexOutOfBounds_caught() {
         Command c = Parser.parse("deadline submit");
         assertInstanceOf(NullCommand.class, c);
-        assertEquals("You must specify a complete by timing for a deadline task using the \"by\" tag!\r\n", outContent.toString());
+        CommandResult cr = c.execute(dummyTaskList, dummyStorage);
+        assertEquals("You must specify a complete by timing for a deadline task using the \"by\" tag!",
+                cr.getResponse());
     }
 }
